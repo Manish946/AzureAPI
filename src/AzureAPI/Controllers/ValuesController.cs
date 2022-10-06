f51eed8cdcb2d457c6aa8563f2478e0271edd882
Manish946 <manishestha@gmail.com>
2022-10-06 20:13:57 +0200

    New value updated.

--- src/AzureAPI/Controllers/ValuesController.cs
+++ src/AzureAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace AzureAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "dotnet", "playbook","Demo" };
+            return new string[] { "dotnet", "playbook","Demo", "Presentation" };
         }
 
         // GET api/values/5
